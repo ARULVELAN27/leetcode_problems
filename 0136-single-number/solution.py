3837724e0e2e7af39730380f55140630971aeabe
ARULVELAN A <arulvelanayyappan@gmail.com>
2024-02-24 13:59:22 +0000

    [LeetCode Sync] Runtime - 3265 ms (12.13%), Memory - 13.7 MB (94.49%)

---
+++ 0136-single-number/solution.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        for i in range(len(nums)):
+            if(nums.count(nums[i])==1):
+                return nums[i]
+        
+        
+        
+                  
+                    
+                
+
+
+
+                 
+
+
+
+        
+            
+                
+                
+               
+
+                
