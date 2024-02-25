44c92b4e6029eb78ba2a1670aad97cbcdbcb6018
ARULVELAN A <arulvelanayyappan@gmail.com>
2024-02-25 18:32:07 +0000

    [LeetCode Sync] Runtime - 14 ms (1.24%), Memory - 11.7 MB (100.00%)

---
+++ 0551-student-attendance-record-i/solution.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def checkRecord(self, s):
+        if "LLL" in s or s.count('A')>=2:
+            return(False)
+        else:
+            return(True)
+        
+        
