6f3485d9d524d7221d4216fb034f30018e570a13
ARULVELAN A <arulvelanayyappan@gmail.com>
2024-03-03 17:22:59 +0000

    [LeetCode Sync] Runtime - 11 ms (0.76%), Memory - 11.7 MB (100.00%)

---
+++ 2491-smallest-even-multiple/solution.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def smallestEvenMultiple(self, n):
+        if n%2==0:
+            return n
+        else:
+            d=2*n
+            return d
+            
+        
