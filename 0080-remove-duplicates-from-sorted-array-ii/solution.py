8bccdf9ac2d5cae4d2a6154b78f42ccb8e5ce5c5
ARULVELAN A <arulvelanayyappan@gmail.com>
2024-03-12 15:24:40 +0000

    [LeetCode Sync] Runtime - 41 ms (100.00%), Memory - 11.9 MB (100.00%)

---
+++ 0080-remove-duplicates-from-sorted-array-ii/solution.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        for x in nums:
+            if nums.count(x)>2:
+                nums.remove(x)
+        for x in nums:
+            if nums.count(x)>2:
+                nums.remove(x)
+        return len(nums)
+        
