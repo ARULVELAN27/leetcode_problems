d8527973d7f391ae663d97ae3e7628afe1e67b48
ARULVELAN A <arulvelanayyappan@gmail.com>
2024-03-30 17:28:06 +0000

    [LeetCode Sync] Runtime - 60 ms (11.73%), Memory - 11.7 MB (100.00%)

--- 0372-super-pow/solution.py
+++ 0372-super-pow/solution.py
@@ -1,17 +1,5 @@
 class Solution(object):
     def superPow(self, a, b):
-        v=len(b)
-        if 1 <= a and a <=(pow(2,31)-1):
-            if 1 <= v and v <= 2000:
-                q = int("".join(map(str, b)))
-                t=pow(a,q,1337)
-                return t
-            else:
-                return 0
-        else:
-            return 0
-
-            
-        
-        
-        
+        y= int("".join(map(str, b)))
+        s=pow(a,y,1337)
+        return s
