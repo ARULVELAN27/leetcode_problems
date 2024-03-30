879a859bdfda19467e5c0c9877adff4a5d0a3680
ARULVELAN A <arulvelanayyappan@gmail.com>
2024-03-30 12:58:07 +0000

    [LeetCode Sync] Runtime - 22 ms (1.39%), Memory - 11.8 MB (100.00%)

---
+++ 0033-search-in-rotated-sorted-array/solution.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def search(self, nums, target):
+        for x in range(0,len(nums)):
+            if nums[x]==target:
+                return x
+        return -1
+        
