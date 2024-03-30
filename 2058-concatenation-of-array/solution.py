5c3f84a5eca876154174eea9e308eaccfe5b5dca
ARULVELAN A <arulvelanayyappan@gmail.com>
2024-03-30 12:48:36 +0000

    [LeetCode Sync] Runtime - 48 ms (5.62%), Memory - 12 MB (100.00%)

---
+++ 2058-concatenation-of-array/solution.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        ans=[]
+        for x in range(0,2):
+            for x in nums:
+                ans.append(x)
+        return ans
+        
