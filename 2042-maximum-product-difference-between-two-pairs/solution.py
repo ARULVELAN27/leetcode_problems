7171341f3026b873b95551c1091fc8073ca38c77
ARULVELAN A <arulvelanayyappan@gmail.com>
2024-04-06 16:59:30 +0000

    [LeetCode Sync] Runtime - 138 ms (58.66%), Memory - 12.4 MB (100.00%)

---
+++ 2042-maximum-product-difference-between-two-pairs/solution.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def maxProductDifference(self, nums):
+        nums.sort(reverse=True)
+        a=(nums[0]*nums[1])-(nums[len(nums)-1]*nums[len(nums)-2])
+        return a
+
