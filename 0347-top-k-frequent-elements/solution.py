0a4f6e907b9c54a597f6d126febf688aef4ceff4
ARULVELAN A <arulvelanayyappan@gmail.com>
2024-04-20 03:35:23 +0000

    [LeetCode Sync] Runtime - 71 ms (7.53%), Memory - 19.4 MB (26.57%)

---
+++ 0347-top-k-frequent-elements/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        count={}
+        freq=[[] for i in range(len(nums)+1)]
+        for n in nums:
+            count[n]=1+count.get(n,0)
+        for n,c in count.items():
+            freq[c].append(n)
+        res=[]
+        for x in range(len(freq)-1,0,-1):
+            for j in freq[x]:
+                res.append(j)
+                if len(res)==k:
+                    return res 
+
+            
+        
