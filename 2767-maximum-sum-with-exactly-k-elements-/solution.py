4deccc6cc10c97975b9aec258d26d40bf752e7d9
ARULVELAN A <arulvelanayyappan@gmail.com>
2024-04-21 04:29:26 +0000

    [LeetCode Sync] Runtime - 177 ms (5.59%), Memory - 11.5 MB (100.00%)

---
+++ 2767-maximum-sum-with-exactly-k-elements-/solution.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def maximizeSum(self, nums, k):
+        a=0
+        b=max(nums)
+        for x in range(1,k+1):
+            a=a+max(nums)
+            nums.remove(max(nums))
+            nums.append(b+x)
+        return a
+
+        
