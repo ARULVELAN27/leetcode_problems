93ddeb0099d24d84d3e1810bda0fed4c9ac4bbd1
ARULVELAN A <arulvelanayyappan@gmail.com>
2024-07-21 09:50:17 +0000

    [LeetCode Sync] Runtime - 25 ms (9.00%), Memory - 11.8 MB (100.00%)

---
+++ 2727-number-of-senior-citizens/solution.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def countSeniors(self, details):
+        a=0
+        for x in details:
+            b=int(x[11])*10+int(x[12])
+            if b>60:
+                a=a+1
+        return a
+
+        
