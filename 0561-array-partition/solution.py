bcc6ae98b4bc211bdb19f4b93bba0204a867de83
ARULVELAN A <arulvelanayyappan@gmail.com>
2025-01-30 02:32:05 +0000

    [LeetCode Sync] Runtime - 35 ms (68.77%), Memory - 13.9 MB (56.85%)

--- 0561-array-partition/solution.py
+++ 0561-array-partition/solution.py
@@ -1,6 +1,13 @@
 class Solution(object):
     def arrayPairSum(self, nums):
-        nums.sort()
-        a=sum(nums[0::2])
-        return a
+        nums.sort(reverse=True)
+        e=0
+        for x in range(0,len(nums),2):
+            e=e+min(nums[x],nums[x+1])
+        return e
+                                                  
+            
+
+
+
         
