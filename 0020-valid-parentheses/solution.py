b68950db06c4562e84bfafebdc8b8a1d89f6b7a9
ARULVELAN A <arulvelanayyappan@gmail.com>
2025-06-07 21:22:31 +0000

    [LeetCode Sync] Runtime - 3 ms (74.23%), Memory - 12.6 MB (11.42%)

--- 0020-valid-parentheses/solution.py
+++ 0020-valid-parentheses/solution.py
@@ -1,17 +1,32 @@
 class Solution(object):
     def isValid(self, s):
-        l = []
-        if len(s)==1:
-            return False
+        l=[]
         for x in s:
             if len(l)==0:
                 l.append(x)
-            elif (x == ")" and l[len(l) - 1] == "(") or (x == "}" and l[len(l) - 1] == "{") or (x == "]" and l[len(l) - 1] == "["):
-                l.pop()
             else:
-                l.append(x)
+                if x==")":
+                    if l[len(l)-1]=="(":
+                        l.pop()
+                    else:
+                        return False
+                elif x=="}":
+                    if l[len(l)-1]=="{":
+                        l.pop()
+                    else:
+                        return False
+                elif x=="]":
+                    if l[len(l)-1]=="[":
+                        l.pop()
+                    else:
+                        return False
+                else:
+                    l.append(x)
         if len(l)==0:
             return True
         return False
+                
+
+
 
         
