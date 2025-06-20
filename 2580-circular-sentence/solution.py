47e537c856ec2b3bf784edd0ea104bccfd6e97d8
ARULVELAN A <arulvelanayyappan@gmail.com>
2025-06-20 02:53:42 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.4 MB (21.18%)

--- 2580-circular-sentence/solution.py
+++ 2580-circular-sentence/solution.py
@@ -1,9 +1,8 @@
 class Solution(object):
-    def isCircularSentence(self, a):
-        a = a.split(" ")
-        a.append(a[0])
-        for x in range(0,len(a)-1):
-            if a[x][len(a[x])-1]!=a[x+1][0]:
+    def isCircularSentence(self, s):
+        a=s.split(" ")
+        for x in range(len(a)):
+            if a[x][len(a[x])-1]!=a[(x+1)%len(a)][0]:
                 return False
         return True
         
