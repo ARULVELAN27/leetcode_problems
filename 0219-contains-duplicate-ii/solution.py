4b24236f5b24f328a8b3e14f765a1330ffeb8691
ARULVELAN A <arulvelanayyappan@gmail.com>
2025-11-15 15:10:41 +0000

    [LeetCode Sync] Runtime - 31 ms (77.49%), Memory - 24 MB (64.68%)

--- 0219-contains-duplicate-ii/solution.py
+++ 0219-contains-duplicate-ii/solution.py
@@ -1,8 +1,9 @@
 class Solution(object):
     def containsNearbyDuplicate(self, nums, k):
-        s={}
-        for x in range(0,len(nums)):
-            if nums[x] in s and abs(x-s[nums[x]])<=k:
-                return True
-            s[nums[x]]=x
+        a={}
+        for i,x in enumerate(nums):
+            if x in a:
+                if i-a[x]<=k:
+                    return True
+            a[x]=i
         return False
