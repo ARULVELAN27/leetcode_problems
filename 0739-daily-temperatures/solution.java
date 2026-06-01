14f8139b82068f393e9b6af958a4cae3f8f3ba61
ARULVELAN A <arulvelanayyappan@gmail.com>
2026-06-01 04:24:48 +0000

    [LeetCode Sync] Runtime - 76 ms (33.80%), Memory - 106.9 MB (50.66%)

---
+++ 0739-daily-temperatures/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] dailyTemperatures(int[] t) {
+
+        int[] arr=new int[t.length];
+
+        Stack<Integer> st=new Stack<>();
+
+        for(int i=t.length-1;i>=0;i--){
+
+            while(!st.isEmpty() && t[st.peek()]<=t[i]){
+                st.pop();
+            }
+
+            if(st.isEmpty()){
+                arr[i]=0;
+            }
+            else{
+                arr[i]=st.peek()-i;
+            }
+
+            st.push(i);
+        }
+
+        return arr;
+    }
+}
